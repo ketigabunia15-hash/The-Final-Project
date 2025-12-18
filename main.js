f4a8524d8a8457fe9fd849b661f8239a93006bdf
ketigabunia15-hash <ketigabunia15@gmail.com>
2025-12-18 11:09:37 +0400

    slider

--- main.js
+++ main.js
@@ -1,57 +1,19 @@
-const links = document.querySelectorAll('a[href^="#"]');
+const images = [
+  "css/img/me.jpg",
+  "css/img/photo1.jpg",
+  "css/img/photo2.jpg"
+];
 
-links.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+let currentIndex = 0;
+const heroImage = document.getElementById("heroImage");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const images = [
-        "css/img/photo1.jpg",
-        "css/img/photo2.jpg",
-    ];
+setInterval(() => {
+  heroImage.style.opacity = 0;
 
-    let currentIndex = 0;
-    const slider = document.getElementById("slider");
+  setTimeout(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    heroImage.src = images[currentIndex];
+    heroImage.style.opacity = 1;
+  }, 500);
 
-    function changeImage() {
-        currentIndex++;
-        if (currentIndex >= images.length) currentIndex = 0;
-        slider.src = images[currentIndex];
-    }
-
-    setInterval(changeImage, 5000);
-});
-
-const progressBars = document.querySelectorAll('.skill-progress');
-function animateProgress() {
-    progressBars.forEach(bar => {
-        const barTop = bar.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-        const progressValue = bar.getAttribute('data-progress');
-
-        if (barTop < windowHeight - 50) { // -50 პატარა offset
-            bar.style.width = progressValue;
-        }
-    });
-}
-
-window.addEventListener('scroll', animateProgress);
-window.addEventListener('load', animateProgress);
-
-const skills = document.querySelectorAll('.skill-progress');
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.style.width = entry.target.dataset.level;
-    }
-  });
-}, { threshold: 0.5 });
-
-skills.forEach(skill => observer.observe(skill));
+}, 5000);
