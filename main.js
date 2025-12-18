e5d7aed6a51470d2b15a488ce06f8ce8e99b1373
ketigabunia15-hash <ketigabunia15@gmail.com>
2025-12-18 22:22:46 +0400

    Update main.js

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ const testimonials = [
   {
     img: "css/img/pic4.jpg",
     quote: "Thank you for the high-quality website design. The standard of the work represents a clear example of professionalism, and I greatly appreciate your efforts.",
-    name: "Tornike Reinovi",
+    name: "Tornike Revinovi",
     position: "Assembly Technician"
   }
 ];
